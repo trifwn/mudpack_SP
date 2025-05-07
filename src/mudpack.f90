591a07a353692424768229e3afff32bc2e5b46ea
Tryfonas Themas <tryfonthem@gmail.com>
2025-05-07 20:23:49 +0300

    Change module name

--- src/mudpack.f90
+++ src/mudpack.f90
@@ -1,7 +1,7 @@
 !*==MUDPACK.f90 processed by SPAG 8.04DB 19:24  7 May 2025
 !!SPAG Open source Personal, Educational or Academic User National Technical University  NON-COMMERCIAL USE - Not for use on proprietary or closed source code
  
-module mudpack
+module mudpack_sp
    implicit none
    public
  
@@ -10,4 +10,4 @@ contains
       include 'mudcom.f90'
       ! use i_mud3sp
       include 'mud3sp.f90'
-end module mudpack
+end module mudpack_sp
